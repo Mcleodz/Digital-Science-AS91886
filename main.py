a6613f3050ab82539cbcc62d7c9659d0d1910746
Mcleodz <mcleodz@stu.otc.school.nz>
2023-06-23 11:01:24 +1200

    finished home page and trying frames

--- main.py
+++ main.py
@@ -1,20 +1,50 @@
 from tkinter import *
 import json
 
+
+"""Create Main Root"""
 root = Tk()
 root.title("Aterio")
 root.config(bg="#F2EFE9")
 root.attributes("-fullscreen", True)
 
-aterio_text = Button(root, text="Aterio")
+"""Home page creation code"""
+def load_home_page(root):
+    # Makes home frame for hompage GUI
+    home_page = Frame(root)
+    home_page.pack()
 
-table_manager_button = Button(root, text="Table Manager", command=lambda:print("hi"), width=84, height=5, bg="#655A7C", fg="#F2EFE9", font=("Arial", 30))
-table_manager_button.place(x=0, y=303)
+    # Makes program name text
+    aterio_text = Label(home_page, text="Aterio", width=71, height=6, bg="#F06233", fg="#F2EFE9", font=("Arial", 35))
+    aterio_text.pack()
 
-reservation_manager_button = Button(root, text="Reservation manager", command=lambda:print("hi"), width=84, height=5, bg="#655A7C", fg="#F2EFE9", font=("Arial", 30))
-reservation_manager_button.place(x=0, y=562)
+    # Makes button to load table manager software
+    table_manager_button = Button(home_page, text="Table Manager", command=lambda:load_table_managment(root, home_page), width=84, height=5, bg="#655A7C", fg="#F2EFE9", font=("Arial", 30))
+    table_manager_button.pack()
 
-quit_button = Button(root, text= "Quit", command=lambda:quit(), width=84, height=5, bg="#690500", fg="#F2EFE9", font=("Arial", 30))
-quit_button.place(x=0, y=821)
+    # Makes button to load reservation manager software
+    reservation_manager_button = Button(home_page, text="Reservation manager", command=lambda:print("hi"), width=84, height=5, bg="#655A7C", fg="#F2EFE9", font=("Arial", 30))
+    reservation_manager_button.pack()
+
+    # Makes button to quit program
+    quit_button = Button(home_page, text= "Quit", command=lambda:quit(), width=84, height=5, bg="#690500", fg="#F2EFE9", font=("Arial", 30))
+    quit_button.pack()
+
+"""Close page"""
+def close_page(root, page_to_close):
+    page_to_close.destroy()
+    load_home_page(root)
+
+"""Table Manager code"""
+def load_table_managment(root, home_page):
+    # Makes table manager page and destroys old page
+    home_page.destroy()
+    table_management_page = Frame(root)
+    table_management_page.pack()
+
+    back_button = Button(table_management_page, text="Back", command=lambda:close_page(root, page_to_close=table_management_page))
+    back_button.place(x=0, y=0)
+
+load_home_page(root)
 
 root.mainloop()
